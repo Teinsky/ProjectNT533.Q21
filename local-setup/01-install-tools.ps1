89f386486da13b143b3e274035ca501e3e820d1e
Phạm Lê Chuẩn <163868527+22520160@users.noreply.github.com>
2026-04-18 09:28:16 +0700

    Delete local-setup directory

--- local-setup/01-install-tools.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Đang cài đặt kubectl..."
-winget install Kubernetes.kubectl
-
-Write-Host "Đang cài đặt helm..."
-winget install Helm.Helm
-
-Write-Host "Kiểm tra phiên bản:"
-kubectl version --client
-helm version
